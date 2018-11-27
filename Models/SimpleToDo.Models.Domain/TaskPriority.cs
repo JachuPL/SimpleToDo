14015757158f799e6a5ccb5f4e55c6f0442b56e0
JachuPL <bonkowskijan@gmail.com>
2018-11-27 20:51:04 +0100

    Dropdowns for enum

--- Models/SimpleToDo.Models.Domain/TaskPriority.cs
+++ Models/SimpleToDo.Models.Domain/TaskPriority.cs
@@ -1,9 +1,16 @@
-﻿namespace SimpleToDo.Models.Domain
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleToDo.Models.Domain
 {
     public enum TaskPriority
     {
+        [Display(Name = "Normalny")]
         Normal,
+
+        [Display(Name = "Wysoki")]
         High,
+
+        [Display(Name = "Krytyczny")]
         Critical
     }
 }
