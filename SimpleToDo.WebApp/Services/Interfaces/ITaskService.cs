806dffe78105861c20fd67e886639fb53d5658fe
JachuPL <bonkowskijan@gmail.com>
2018-11-27 11:43:48 +0100

    Task service stub with DI container configuration

---
+++ SimpleToDo.WebApp/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,11 @@
+﻿using SimpleToDo.WebApp.Models.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.WebApp.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<List<ToDoTask>> GetPage(int page, int tasksPerPage);
+    }
+}
---
+++ SimpleToDo.WebApp/Services/TaskService.cs
@@ -0,0 +1,16 @@
+﻿using SimpleToDo.WebApp.Models.Domain;
+using SimpleToDo.WebApp.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleToDo.WebApp.Services
+{
+    public class TaskService : ITaskService
+    {
+        public Task<List<ToDoTask>> GetPage(int page, int tasksPerPage)
+        {
+            List<ToDoTask> mockList = new List<ToDoTask>();
+            return Task.FromResult(mockList);
+        }
+    }
+}
