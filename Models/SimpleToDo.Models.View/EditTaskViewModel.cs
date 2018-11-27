b67d0dfc8526b58ee052c5e4d5896e8edb7ef96d
JachuPL <bonkowskijan@gmail.com>
2018-11-27 20:52:13 +0100

    Display meaningful field names

--- Models/SimpleToDo.Models.View/EditTaskViewModel.cs
+++ Models/SimpleToDo.Models.View/EditTaskViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using SimpleToDo.Models.Domain;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace SimpleToDo.Models.View
 {
     public class EditTaskViewModel
     {
+        [Display(Name = "Tytuł")]
         public string Title { get; set; }
+
+        [Display(Name = "Opis")]
         public string Description { get; set; }
+
+        [Display(Name = "Przewid. termin zakończenia")]
         public DateTime DueDate { get; set; }
+
+        [Display(Name = "Status")]
         public bool Finished { get; set; }
+
+        [Display(Name = "Priorytet")]
         public TaskPriority Priority { get; set; }
     }
 }
