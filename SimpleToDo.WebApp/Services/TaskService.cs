8bfe4468de828f56d43e9400ea7294d9439046ea
JachuPL <bonkowskijan@gmail.com>
2018-11-27 11:50:12 +0100

    Mock list object filler

--- SimpleToDo.WebApp/Services/TaskService.cs
+++ SimpleToDo.WebApp/Services/TaskService.cs
@@ -1,4 +1,4 @@
-﻿using SimpleToDo.WebApp.Models.Domain;
+using SimpleToDo.WebApp.Models.Domain;
 using SimpleToDo.WebApp.Services.Interfaces;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -10,7 +10,16 @@ namespace SimpleToDo.WebApp.Services
         public Task<List<ToDoTask>> GetPage(int page, int tasksPerPage)
         {
             List<ToDoTask> mockList = new List<ToDoTask>();
+            FillWithSomeObjects(mockList);
             return Task.FromResult(mockList);
+        }
+
+        private void FillWithSomeObjects(List<ToDoTask> mockList)
+        {
+            Random r = new Random();
+            int max = r.Next(10, 20);
+            for (int i = 0; i < max; i++)
+                mockList.Add(new ToDoTask());
         }
     }
 }
