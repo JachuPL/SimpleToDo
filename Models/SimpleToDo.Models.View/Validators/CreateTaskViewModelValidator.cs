1b4964ca6e4b4d2351606730b5b45fbb0a48ee7b
JachuPL <bonkowskijan@gmail.com>
2018-11-28 09:28:12 +0100

    Added FluentValidator dependency

---
+++ Models/SimpleToDo.Models.View/Validators/CreateTaskViewModelValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using SimpleToDo.Models.Domain;
+
+namespace SimpleToDo.Models.View.Validators
+{
+    public class CreateTaskViewModelValidator : AbstractValidator<CreateTaskViewModel>
+    {
+        public CreateTaskViewModelValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Wprowadź nazwę zadania.")
+                .MinimumLength(2).WithMessage("Nazwa zadania powinna zawierać co najmniej 2 znaki.")
+                .MaximumLength(100).WithMessage("Nazwa zadania powinna zawierać maksymalnie 100 znaków.");
+
+            RuleFor(x => x.Description)
+                .MinimumLength(2).WithMessage("Opis zadania powinien zawierać co najmniej 2 znaki.");
+
+            RuleFor(x => x.DueDate)
+                .NotEmpty().WithMessage("Podaj przewidywaną datę zakończenia zadania.");
+
+            RuleFor(x => x.Priority)
+                .Must(BeAValidValue)
+                .WithMessage(
+                    "Wartościami dozwolonymi dla priorytetu zadania są \"Normalny\", \"Wysoki\" oraz \"Krytyczny\".");
+        }
+
+        private bool BeAValidValue(TaskPriority parsedPriority)
+        {
+            return TaskPriority.Normal <= parsedPriority && parsedPriority <= TaskPriority.Critical;
+        }
+    }
+}
