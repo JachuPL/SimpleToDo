eb519a81e9ea5abba7e0d954c051284293099071
JachuPL <bonkowskijan@gmail.com>
2018-11-28 08:31:04 +0100

    Fixed datetimepickers on Create and Edit, added custom datetime modelbinders

---
+++ SimpleToDo.WebApp/Binders/DateTimeModelBinderProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace SimpleToDo.WebApp.Binders
+{
+    public class DateTimeModelBinderProvider : IModelBinderProvider
+    {
+        private readonly IModelBinder binder = new DateTimeModelBinder("dd.MM.yyyy HH:mm:ss");
+
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            return context.Metadata.ModelType == typeof(DateTime) ? binder : null;
+        }
+    }
+}
