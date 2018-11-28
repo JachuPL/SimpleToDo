4ab584329a6ab190f86c2da5f16fb67fdaefc794
JachuPL <bonkowskijan@gmail.com>
2018-11-28 14:17:36 +0100

    Added AutoMapper project with configuration

---
+++ SimpleToDo.AutoMapper/ToDoTaskProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using SimpleToDo.Models.Domain;
+using SimpleToDo.Models.View;
+
+namespace SimpleToDo.AutoMapper
+{
+    public class ToDoTaskProfile : Profile
+    {
+        public ToDoTaskProfile()
+        {
+            CreateMap<ToDoTask, TaskIndexViewModel>();
+            CreateMap<ToDoTask, TaskDetailsViewModel>();
+            CreateMap<EditTaskViewModel, ToDoTask>();
+        }
+    }
+}
