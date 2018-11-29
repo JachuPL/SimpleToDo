e9ba509780d261f05299314b195fb285bd5f0556
JachuPL <bonkowskijan@gmail.com>
2018-11-29 09:51:33 +0100

    Fixed viewmodel and views to use view-related enum

--- Models/SimpleToDo.Models.View/TaskIndexViewModel.cs
+++ Models/SimpleToDo.Models.View/TaskIndexViewModel.cs
@@ -6,6 +6,6 @@ namespace SimpleToDo.Models.View
     public class TaskIndexViewModel
     {
         public IPagedList<TaskListElementViewModel> Tasks { get; set; }
-        public TaskStatus FilteredStatus { get; set; }
+        public FilteredTaskStatus FilteredStatus { get; set; }
     }
 }
